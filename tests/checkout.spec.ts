d04cc2ae2deecf34ce38c2ff644b7ab0668fa51e
Aswin Cp <aswincp715@gmail.com>
2026-07-19 22:13:14 +0530

    final fix

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 import { InventoryPage } from "../pages/InventoryPage";
 import { CartPage } from "../pages/CartPage";
-import { CheckoutPage } from "../pages/CheckoutPageTemp";
+import { CheckoutPage } from "../pages/CheckoutPage";
 
 test.describe("SauceDemo Checkout", () => {
     let inventoryPage: InventoryPage;
